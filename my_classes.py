8001c875e9c77ae233e295018a89b052a61edd2b
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2023-11-22 16:06:40 +0400

    refactor to Class(OOP) structure

--- my_classes.py
+++ my_classes.py
@@ -5,6 +5,12 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from datetime import datetime, timedelta
 
+DEFAULT_DB_FOLDER = "E:\_WORK\db"
+PAD_MAX = 10
+PAD_MIN = 5
+WINDOW_SIZE = "500x600"
+OPTIONS = ["Combo", "Database", "Logs", "Mixed"]
+
 
 class FileManagerApp:
     def __init__(self, master):
@@ -12,7 +18,8 @@ class FileManagerApp:
         master.title("Leeks file manager")
         master.geometry("500x600")
 
-        self.DEFAULT_DB_FOLDER = "E:/_WORK/TESTS"
+        self.DEFAULT_DB_FOLDER = DEFAULT_DB_FOLDER
+        self.db_folder = tk.StringVar(value=DEFAULT_DB_FOLDER)
         self.PAD_MAX = 10
         self.PAD_MIN = 5
 
@@ -21,17 +28,31 @@ class FileManagerApp:
         self.file_path_var = tk.StringVar()
         self.date_var = tk.StringVar(value=datetime.now().strftime("%Y-%m-%d"))
         self.domain_value = tk.StringVar()
-        self.text1_content = tk.StringVar()
-        self.text2_content = tk.StringVar()
-        self.text3_content = tk.StringVar()
+
+        self.link_value = tk.StringVar()
+        self.topic_value = tk.StringVar()
+
+        self.readme_value = tk.StringVar()
+        self.password_value = tk.StringVar()
 
         # Create widgets
         self.create_widgets()
 
     def create_widgets(self):
         # Listbox
-        self.listbox = tk.Listbox(self.master, height=4, listvariable=self.selected_value, selectmode=tk.SINGLE)
-        self.listbox.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=0, column=0, rowspan=4, sticky=tk.W + tk.E)
+
+        # List
+        options = OPTIONS
+        self.listbox = tk.Listbox(self.master, height=4)
+        for option in options:
+            self.listbox.insert(tk.END, option)
+        self.listbox.select_set(0)
+        # print(listbox.get(tk.ACTIVE))
+        self.listbox.grid(padx=PAD_MAX, pady=PAD_MIN, row=0, column=0, rowspan=4, sticky=tk.W + tk.E)
+        # listbox.pack(padx=20, pady=20, )
+
+        # self.listbox = tk.Listbox(self.master, height=4, listvariable=self.selected_value, selectmode=tk.SINGLE)
+        # self.listbox.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=0, column=0, rowspan=4, sticky=tk.W + tk.E)
 
         # File Chooser
         file_button = tk.Button(self.master, text="Choose File", command=self.choose_file)
@@ -58,32 +79,43 @@ class FileManagerApp:
         buttonframe.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=2, column=1, sticky=tk.W + tk.E)
 
         # Domain Entry
-        domein_label = tk.Label(self.master, text="Domain name(if exist)/Folder name: ")
-        domein_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=6, column=0, sticky=tk.W + tk.E)
+        domain_label = tk.Label(self.master, text="Domain name(if exist)/Folder name: ")
+        domain_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=6, column=0, sticky=tk.W + tk.E)
 
-        domein_entry = tk.Entry(self.master, width=30, textvariable=self.domain_value)
-        domein_entry.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=6, column=1, sticky=tk.W + tk.E)
+        domain_entry = tk.Entry(self.master, width=30, textvariable=self.domain_value)
+        domain_entry.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=6, column=1, sticky=tk.W + tk.E)
+
+        # Link Entry
+        topic_link_label = tk.Label(self.master, text="Forum / TG link:")
+        topic_link_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=7, column=0, sticky=tk.W + tk.E)
+
+        text_topic_link = tk.Entry(self.master, width=30, textvariable=self.link_value)
+        text_topic_link.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=7, column=1, sticky=tk.W + tk.E)
+
+        # Topic Entry
+        topic_name_label = tk.Label(self.master, text="Topic name / TG channel name:")
+        topic_name_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=8, column=0, sticky=tk.W + tk.E)
+
+        text_topic_name = tk.Entry(self.master, width=30, textvariable=self.topic_value)
+        text_topic_name.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=8, column=1, sticky=tk.W + tk.E)
 
         # Text Readme
-        readme_file_label = tk.Label(self.master, text="Readme File Content: ")
-        readme_file_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=7, column=0, sticky=tk.W + tk.E)
-
-        text_readme = tk.Text(self.master, height=4, width=40)
-        text_readme.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=9, column=0, columnspan=2, sticky=tk.W + tk.E)
+        # text_readme = tk.Text(self.master, height=4, width=40)
+        # text_readme.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=9, column=0, columnspan=2, sticky=tk.W + tk.E)
 
         # Password Entry
         pass_label = tk.Label(self.master, text="Password(if exist): ")
         pass_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=10, column=0, sticky=tk.W + tk.E)
 
-        text_pass = tk.Entry(self.master, width=30, textvariable=self.text2_content)
+        text_pass = tk.Entry(self.master, width=30, textvariable=self.password_value)
         text_pass.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=10, column=1, sticky=tk.W + tk.E)
 
         # Folder Entry
-        folder_label = tk.Label(self.master, text="Data Base(db) folder path: ")
-        folder_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=11, column=0, sticky=tk.W + tk.E)
-
-        text_folder = tk.Entry(self.master, width=30)
-        text_folder.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=11, column=1, sticky=tk.W + tk.E)
+        # folder_label = tk.Label(self.master, text="Data Base(db) folder path: ")
+        # folder_label.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=11, column=0, sticky=tk.W + tk.E)
+        #
+        # text_folder = tk.Entry(self.master, width=30, textvariable=self.db_folder)
+        # text_folder.grid(padx=self.PAD_MAX, pady=self.PAD_MIN, row=11, column=1, sticky=tk.W + tk.E)
 
         # Submit button
         submit_button = tk.Button(self.master, text="Submit", command=self.submit)
@@ -94,7 +126,7 @@ class FileManagerApp:
         self.file_path_var.set(file_path)
         self.file_name_label.config(text=file_path)
         file_name, file_extension = os.path.splitext(os.path.basename(file_path))
-        self.domein_value.set(os.path.basename(file_name))
+        self.domain_value.set(os.path.basename(file_name))
 
     def increment_date(self):
         current_date = datetime.strptime(self.date_var.get(), "%Y-%m-%d")
@@ -107,25 +139,10 @@ class FileManagerApp:
         self.date_var.set(new_date.strftime("%Y-%m-%d"))
 
     def submit(self):
-        selected = self.listbox.get(tk.ACTIVE)
-        file_path = self.file_path_var.get()
-        date_value = self.date_var.get()
-        text1_content = self.text1_content.get()
-        text2_content = self.text2_content.get()
-        text3_content = self.domain_value.get()
 
-        print("Selected from list:", selected)
-        print("File path:", file_path)
-        print("Date:", date_value)
-        print("Text 1 content:", text1_content)
-        print("Text 2 content:", text2_content)
-        print("Text 3 content:", text3_content)
-
-        leak_name_folder = self.create_directory_structure(selected, date_value, text1_content, text2_content, text3_content)
-        print(leak_name_folder)
-        self.remove_file_to_leak_folder(file_path, leak_name_folder)
-        result_message = leak_name_folder
-        messagebox.showinfo(title="Message", message="File replaced to: " + result_message)
+        leak_name_folder = self.create_directory_structure()
+        self.remove_file_to_leak_folder(leak_name_folder)
+        messagebox.showinfo(title="Message", message="File replaced to: " + leak_name_folder)
 
     def is_valid_folder_name(self, name):
         # Define a regular expression for a valid folder name
@@ -145,14 +162,19 @@ class FileManagerApp:
 
             os.makedirs(folder_name)
 
-    def create_directory_structure(self, selected_value, date_value, leak_name_value, pass_text, domain_text):
+    def create_directory_structure(self):
+        selected = self.listbox.get(tk.ACTIVE)
+        domain_text = self.domain_value.get()
+        leak_name_value = f"{self.topic_value.get()}\n{self.link_value.get()}\n"
+        pass_text = self.password_value.get()
+
         db_folder = self.DEFAULT_DB_FOLDER
-        listbox_folder = os.path.join(db_folder, selected_value)
-        date_folder = os.path.join(listbox_folder, date_value)
+        listbox_folder = os.path.join(db_folder, selected)
+        date_folder = os.path.join(listbox_folder, self.date_var.get())
         if domain_text:
             leak_name_folder = os.path.join(date_folder, domain_text)
         else:
-            leak_name_folder = os.path.join(date_folder, leak_name_value.split("\n")[0])
+            leak_name_folder = os.path.join(date_folder, self.topic_value.get())
 
         self.if_directory_not_exist_create_new(db_folder)
         self.if_directory_not_exist_create_new(listbox_folder)
@@ -177,9 +199,9 @@ class FileManagerApp:
 
         return leak_name_folder
 
-    def remove_file_to_leak_folder(self, file_path, leak_name_folder):
-        if file_path and os.path.exists(file_path):
-            shutil.move(file_path, leak_name_folder)
+    def remove_file_to_leak_folder(self, leak_name_folder):
+        if os.path.exists(self.file_path_var.get()):
+            shutil.move(self.file_path_var.get(), leak_name_folder)
 
 
 if __name__ == "__main__":
